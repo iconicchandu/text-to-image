4e3dd132411587b8ec02f185afdd9473e0ac5579
iconicchandu <chandrakant.mau786@gmail.com>
2025-09-17 13:49:32 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Text to Image',
+  description: 'Created with ❤️',
+  generator: 'Iconic Chandu',
 }
 
 export default function RootLayout({
